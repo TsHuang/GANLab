e05ef5b26c431745a6bf489fb1622c9bc0e5f64f
TsHuang <tshuang1983@gmail.com>
2017-12-04 00:30:59 +0800

    update VAEGAN.py

--- VAE_GAN/VAEGAN.py
+++ VAE_GAN/VAEGAN.py
@@ -48,17 +48,17 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--workers', type=int, help='number of data loading workers', default=2)
 parser.add_argument('--batchSize', type=int, default=64, help='input batch size')
 parser.add_argument('--imageSize', type=int, default=64, help='the height / width of the input image to network')
-parser.add_argument('--nz', type=int, default=100, help='size of the latent z vector')
-parser.add_argument('--ngf', type=int, default=64)
-parser.add_argument('--ndf', type=int, default=64)
-parser.add_argument('--niter', type=int, default=25, help='number of epochs to train for')
-parser.add_argument('--lr', type=float, default=0.0002, help='learning rate, default=0.0002')
+parser.add_argument('--nz', type=int, default=2048, help='size of the latent z vector')
+parser.add_argument('--ngf', type=int, default=32)
+parser.add_argument('--ndf', type=int, default=32)
+parser.add_argument('--niter', type=int, default=50, help='number of epochs to train for')
+parser.add_argument('--lr', type=float, default=0.0003, help='learning rate, default=0.0002')
 parser.add_argument('--beta1', type=float, default=0.5, help='beta1 for adam. default=0.5')
 parser.add_argument('--cuda', action='store_true', help='enables cuda')
 parser.add_argument('--ngpu', type=int, default=1, help='number of GPUs to use')
 parser.add_argument('--netG', default='', help="path to netG (to continue training)")
 parser.add_argument('--netD', default='', help="path to netD (to continue training)")
-parser.add_argument('--outf', default='.', help='folder to output images and model checkpoints')
+parser.add_argument('--outf', default='./results', help='folder to output images and model checkpoints')
 parser.add_argument('--manualSeed', type=int, help='manual seed')
 
 opt = parser.parse_args()
@@ -68,6 +68,11 @@ try:
     os.makedirs(opt.outf)
 except OSError:
     pass
+
+# save training loss
+curve_file = opt.outf + '/curveData.csv'
+report = np.array(['Epoch', 'dataIdx', 'Loss_D', 'Loss_G', 'D(x)', 'D(G(z1))', 'D(G(z2))'])
+
 
 if opt.manualSeed is None:
     opt.manualSeed = random.randint(1, 10000)
@@ -228,8 +233,43 @@ class _netG(nn.Module):
         self.sampler.cuda()
         self.decoder.cuda()
 
+# class _netG(nn.Module):
+#     def __init__(self, ngpu):
+#         super(_netG, self).__init__()
+#         self.ngpu = ngpu
+#         self.main = nn.Sequential(
+#             # input is Z, going into a convolution
+#             nn.ConvTranspose2d(nz, ngf * 8, 4, 1, 0, bias=False),
+#             nn.BatchNorm2d(ngf * 8),
+#             nn.ReLU(True),
+#             # state size. (ngf*8) x 4 x 4
+#             nn.ConvTranspose2d(ngf * 8, ngf * 4, 4, 2, 1, bias=False),
+#             nn.BatchNorm2d(ngf * 4),
+#             nn.ReLU(True),
+#             # state size. (ngf*4) x 8 x 8
+#             nn.ConvTranspose2d(ngf * 4, ngf * 2, 4, 2, 1, bias=False),
+#             nn.BatchNorm2d(ngf * 2),
+#             nn.ReLU(True),
+#             # state size. (ngf*2) x 16 x 16
+#             nn.ConvTranspose2d(ngf * 2, ngf, 4, 2, 1, bias=False),
+#             nn.BatchNorm2d(ngf),
+#             nn.ReLU(True),
+#             # state size. (ngf) x 32 x 32
+#             nn.ConvTranspose2d(ngf, nc, 4, 2, 1, bias=False),
+#             nn.Tanh()
+#             # state size. (nc) x 64 x 64
+#         )
+#
+#     def forward(self, input):
+#         if isinstance(input.data, torch.cuda.FloatTensor) and self.ngpu > 1:
+#             output = nn.parallel.data_parallel(self.main, input, range(self.ngpu))
+#         else:
+#             output = self.main(input)
+#         return output
+
 
 netG = _netG(opt.imageSize, ngpu)
+#netG = _netG(ngpu)
 netG.apply(weights_init)
 if opt.netG != '':
     netG.load_state_dict(torch.load(opt.netG))
@@ -269,9 +309,43 @@ class _netD(nn.Module):
 
         return output.view(-1, 1)
 
+# class _netD(nn.Module):
+#     def __init__(self, ngpu):
+#         super(_netD, self).__init__()
+#         self.ngpu = ngpu
+#         self.main = nn.Sequential(
+#             # input is (nc) x 64 x 64
+#             nn.Conv2d(nc, ndf, 4, 2, 1, bias=False),
+#             nn.LeakyReLU(0.2, inplace=True),
+#             # state size. (ndf) x 32 x 32
+#             nn.Conv2d(ndf, ndf * 2, 4, 2, 1, bias=False),
+#             nn.BatchNorm2d(ndf * 2),
+#             nn.LeakyReLU(0.2, inplace=True),
+#             # state size. (ndf*2) x 16 x 16
+#             nn.Conv2d(ndf * 2, ndf * 4, 4, 2, 1, bias=False),
+#             nn.BatchNorm2d(ndf * 4),
+#             nn.LeakyReLU(0.2, inplace=True),
+#             # state size. (ndf*4) x 8 x 8
+#             nn.Conv2d(ndf * 4, ndf * 8, 4, 2, 1, bias=False),
+#             nn.BatchNorm2d(ndf * 8),
+#             nn.LeakyReLU(0.2, inplace=True),
+#             # state size. (ndf*8) x 4 x 4
+#             nn.Conv2d(ndf * 8, 1, 4, 1, 0, bias=False),
+#             nn.Sigmoid()
+#         )
+#
+#     def forward(self, input):
+#         if isinstance(input.data, torch.cuda.FloatTensor) and self.ngpu > 1:
+#             output = nn.parallel.data_parallel(self.main, input, range(self.ngpu))
+#         else:
+#             output = self.main(input)
+#
+#         return output.view(-1, 1).squeeze(1)
+
 
 
 netD = _netD(opt.imageSize, ngpu)
+#netD = _netD(ngpu)
 netD.apply(weights_init)
 if opt.netD != '':
     netD.load_state_dict(torch.load(opt.netD))
@@ -346,7 +420,7 @@ for epoch in range(opt.niter):
         logvar = encoded[1]
 
         KLD_element = mu.pow(2).add_(logvar.exp()).mul_(-1).add_(1).add_(logvar)
-        KLD = torch.sum(KLD_element).mul_(-0.5)
+        KLD = torch.sum(KLD_element).mul_(-0.5 * 15) #Beta = 15
 
         sampled = netG.sampler(encoded)
         rec = netG.decoder(sampled)
@@ -373,6 +447,12 @@ for epoch in range(opt.niter):
         print('[%d/%d][%d/%d] Loss_D: %.4f Loss_G: %.4f D(x): %.4f D(G(z)): %.4f / %.4f'
               % (epoch, opt.niter, i, len(dataloader),
                  errD.data[0], errG.data[0], D_x, D_G_z1, D_G_z2))
+
+        newdata = np.array([epoch, i, errD.data[0], errG.data[0], D_x, D_G_z1, D_G_z2])
+        report = np.vstack((report, newdata))
+
+
+
         if i % 100 == 0:
             vutils.save_image(real_cpu,
                               '%s/real_samples.png' % opt.outf,
@@ -386,3 +466,6 @@ for epoch in range(opt.niter):
     # do checkpointing
     torch.save(netG.state_dict(), '%s/netG_epoch_%d.pth' % (opt.outf, epoch))
     torch.save(netD.state_dict(), '%s/netD_epoch_%d.pth' % (opt.outf, epoch))
+
+#save curve data
+np.savetxt(curve_file, report, fmt="%s", delimiter=",")
