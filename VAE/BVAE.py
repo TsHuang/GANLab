e6b95a7cbf7e490e9d896deae39aae725b8a2222
TsHuang <tshuang1983@gmail.com>
2017-12-17 13:24:02 +0800

    update codes and results

--- VAE/BVAE.py
+++ VAE/BVAE.py
@@ -1,5 +1,6 @@
 from __future__ import print_function
 import argparse
+import os
 import torch
 import torch.utils.data
 from torch import nn, optim
@@ -7,18 +8,19 @@ from torch.autograd import Variable
 from torch.nn import functional as F
 from torchvision import datasets, transforms
 from torchvision.utils import save_image
+import numpy as np
 
 
 parser = argparse.ArgumentParser(description='VAE MNIST Example')
 parser.add_argument('--batch-size', type=int, default=128, metavar='N',
                     help='input batch size for training (default: 128)')
-parser.add_argument('--epochs', type=int, default=10, metavar='N',
-                    help='number of epochs to train (default: 10)')
+parser.add_argument('--epochs', type=int, default=100, metavar='N',
+                    help='number of epochs to train (default: 100)')
 parser.add_argument('--no-cuda', action='store_true', default=False,
                     help='enables CUDA training')
 parser.add_argument('--seed', type=int, default=1, metavar='S',
                     help='random seed (default: 1)')
-parser.add_argument('--log-interval', type=int, default=10, metavar='N',
+parser.add_argument('--log-interval', type=int, default=1, metavar='N',
                     help='how many batches to wait before logging training status')
 args = parser.parse_args()
 args.cuda = not args.no_cuda and torch.cuda.is_available()
@@ -43,10 +45,10 @@ class VAE(nn.Module):
     def __init__(self):
         super(VAE, self).__init__()
 
-        self.fc1 = nn.Linear(784, 400) #28x28, 20x20
-        self.fc21 = nn.Linear(400, 20)
-        self.fc22 = nn.Linear(400, 20)
-        self.fc3 = nn.Linear(20, 400)
+        self.fc1 = nn.Linear(784, 400)
+        self.fc21 = nn.Linear(400, 10) #lentent codes = 10
+        self.fc22 = nn.Linear(400, 10)
+        self.fc3 = nn.Linear(10, 400)
         self.fc4 = nn.Linear(400, 784)
 
         self.relu = nn.ReLU()
@@ -79,7 +81,7 @@ if args.cuda:
     model.cuda()
 
 
-def loss_function(recon_x, x, mu, logvar):
+def loss_function(recon_x, x, mu, logvar,beta):
     BCE = F.binary_cross_entropy(recon_x, x.view(-1, 784))
 
     # see Appendix B from VAE paper:
@@ -87,17 +89,18 @@ def loss_function(recon_x, x, mu, logvar):
     # https://arxiv.org/abs/1312.6114
     # 0.5 * sum(1 + log(sigma^2) - mu^2 - sigma^2)
     #KLD = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
-    KLD = -3 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
+    KLD = -beta * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
     # Normalise by same number of elements as in reconstruction
     KLD /= args.batch_size * 784
 
     return BCE + KLD
 
 
-optimizer = optim.Adam(model.parameters(), lr=1e-3)
+#optimizer = optim.Adam(model.parameters(), lr=1e-3)
+optimizer = optim.RMSprop(model.parameters(), lr=1e-3, alpha=0.99, eps=1e-8, weight_decay=0, momentum=0)
 
 
-def train(epoch):
+def train(epoch, beta):
     model.train()
     train_loss = 0
     for batch_idx, (data, _) in enumerate(train_loader):
@@ -106,7 +109,7 @@ def train(epoch):
             data = data.cuda()
         optimizer.zero_grad()
         recon_batch, mu, logvar = model(data)
-        loss = loss_function(recon_batch, data, mu, logvar)
+        loss = loss_function(recon_batch, data, mu, logvar, beta)
         loss.backward()
         train_loss += loss.data[0]
         optimizer.step()
@@ -116,8 +119,8 @@ def train(epoch):
                 100. * batch_idx / len(train_loader),
                 loss.data[0] / len(data)))
 
-        Traindata = np.array([epoch, i, errD.data[0], errG.data[0], D_x, D_G_z1, D_G_z2])
-        report = np.vstack((report, newdata))
+        #Traindata = np.array([epoch, i, errD.data[0], errG.data[0], D_x, D_G_z1, D_G_z2])
+        #report = np.vstack((report, newdata))
 
     print('====> Epoch: {} Average loss: {:.4f}'.format(
           epoch, train_loss / len(train_loader.dataset)))
@@ -125,7 +128,7 @@ def train(epoch):
 
 
 
-def test(epoch):
+def test(epoch, beta):
     model.eval()
     test_loss = 0
     for i, (data, _) in enumerate(test_loader):
@@ -133,7 +136,7 @@ def test(epoch):
             data = data.cuda()
         data = Variable(data, volatile=True)
         recon_batch, mu, logvar = model(data)
-        test_loss += loss_function(recon_batch, data, mu, logvar).data[0]
+        test_loss += loss_function(recon_batch, data, mu, logvar, beta).data[0]
         if i == 0:
           n = min(data.size(0), 8)
           comparison = torch.cat([data[:n],
@@ -145,12 +148,48 @@ def test(epoch):
     print('====> Test set loss: {:.4f}'.format(test_loss))
 
 
-for epoch in range(1, args.epochs + 1):
-    train(epoch)
-    test(epoch)
-    sample = Variable(torch.randn(64, 20))
+if not os.path.isdir('.//VAE_Results'):
+    os.mkdir('.//VAE_Results')
+if not os.path.isdir('.//VAE_Model'):
+    os.mkdir('.//VAE_Model')
+
+if __name__=='__main__':
+
+    Beta = 6
+
+    for epoch in range(1, args.epochs + 1):
+        train(epoch, Beta)
+        test(epoch, Beta)
+
+
+    torch.save(model, 'VAE_Model/model.pkl')
+
+    # generate random input
+    Z = np.multiply(np.ones((80, 1)), np.random.normal(0, 1, (1, 10)))
+    cnt = 0
+    # add delta to each dimension of the latent codes
+    for j_idx in range(8):
+        for i_idx in np.arange(-2, 2.1, 4.0 / 7.0):
+            Z[cnt, j_idx] = i_idx
+            cnt += 1
+
+    sample = Variable(torch.from_numpy(Z))
+    sample = sample.type(torch.FloatTensor)
+
     if args.cuda:
-       sample = sample.cuda()
-    sample = model.decode(sample).cpu()
-    save_image(sample.data.view(64, 1, 28, 28),
-               'VAEResults/sample_' + str(epoch) + '.png')
+        sample = sample.cuda()
+
+    img = model.decode(sample).cpu()
+    save_image(img.data.view(80,1,28,28),'./VAE_Results/VAE_Entangled.png')
+
+
+#     #generate samples
+#     sample = Variable(torch.randn(64, 20))
+#     if args.cuda:
+#        sample = sample.cuda()
+#     sample = model.decode(sample).cpu()
+#     save_image(sample.data.view(64, 1, 28, 28),
+#                'VAEResults/sample_' + str(epoch) + '.png')
+
+
+
